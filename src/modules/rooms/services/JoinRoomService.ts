888d0edce917ef60c642d71a3823280fd236f100
Mateus Moreira <mateusmoreirav@gmail.com>
2020-08-29 18:41:17 -0300

    Feat: validate the capacity of the room

--- src/modules/rooms/services/JoinRoomService.ts
+++ src/modules/rooms/services/JoinRoomService.ts
@@ -13,12 +13,13 @@ class JoinRoomService {
   }: IRequestDTO): Promise<IRoomsInterface> {
     const room = await Rooms.findOne({ _id: roomId });
 
-    // TODO: Utilizar o limite da sala da capacidade
-
     if (!room) throw new AppError('This Room does not exists!', 404);
 
     if (room.users?.includes(userId))
       throw new AppError('This user is Already in this Room!', 409);
+
+    if (room.capacity === room.users?.length)
+      throw new AppError('The room hit the capacity of users!', 400);
 
     room.users?.push(userId);
 
